b408cf190c0280abf984c47c87617459eb575dea
devAltss <altoss11@gmail.com>
2021-08-12 23:42:29 +0300

    finish project

--- src/js/components/quiz.js
+++ src/js/components/quiz.js
@@ -1,3 +1,6 @@
+let quizFormData = null;
+let textareaText = null;
+
 const quizData = [{
   number: 1,
   title: "Какой тип кроссовок рассматриваете?",
@@ -67,8 +70,6 @@ const quizData = [{
 ];
 
 
-
-
 const quizTemplate = (data = [], dataLength, options) => {
 	const {number, title} = data;
 	const {nextBtnText} = options;
@@ -88,7 +89,6 @@ const quizTemplate = (data = [], dataLength, options) => {
       return `
 			<label class="quiz-question__label">
         <textarea placeholder="${item.answer_title}" class="quiz-question__message"></textarea>
-			
 			</label>
 		`;
     } else {
@@ -112,12 +112,8 @@ const quizTemplate = (data = [], dataLength, options) => {
         <button type="button" class="btn btn-reset btn--thirdly quiz-question__btn" data-next-btn>${nextBtnText}</button>
       </div>
     </div>
-	`;
+	`
 };
-
-
-// const quiz = document.querySelector('.quiz');
-// quiz.innerHTML = quizTemplate(quizData[0], quizData.length);
 
 class Quiz {
 	constructor(selector, data, options) {
@@ -128,8 +124,8 @@ class Quiz {
 		this.dataLength = this.data.length;
 		this.resultArray = [];
 		this.tmp = {};
-		this.init();
-		this.events();
+		this.init()
+		this.events()
 	}
 
 	init() {
@@ -143,41 +139,78 @@ class Quiz {
 				this.addToSend();
 				this.nextQuestion();
 			}
-
-			if (e.target == document.querySelector('[data-send]')) {
-				this.send();
-			}
 		});
 
 		this.$el.addEventListener('change', (e) => {
 			if (e.target.tagName == 'INPUT') {
 				if (e.target.type !== 'checkbox' && e.target.type !== 'radio') {
-					let elements = this.$el.querySelectorAll('input');
+					let elements = this.$el.querySelectorAll('input')
 
-					elements.forEach(el => el.checked = false);
+					elements.forEach(el => {
+            el.checked = false;
+          });
 				}
-
-				this.tmp = this.serialize(this.$el);
-			}
+				this.tmp = this.serialize(document.querySelector('.quiz-form'));
+      } else {
+        let textarea = this.$el.querySelector('textarea');   
+        textareaText = textarea.value;    
+      }
+      
 		});
 	}
 
 	nextQuestion() {
     console.log('next question!');
 		if (this.valid()) {
-			if (this.counter + 1 < this.dataLength) {
-				this.counter++;
+			if (this.counter + 1 < this.dataLength) {   
+				this.counter++;                   
 				this.$el.innerHTML = quizTemplate(quizData[this.counter], this.dataLength, this.options);
 
 				if (this.counter + 1 == this.dataLength) {
-					//this.$el.querySelector('.quiz-bottom').insertAdjacentHTML('beforeend', `<button type="button" class="quiz-question__btn" data-send>${this.options.sendBtnText}</button>`);
-					//this.$el.querySelector('[data-next-btn]').remove();
-
+					document.querySelector('.quiz-question__answers').style.display = 'block';
 				}
 			} else {
         console.log('А всё! конец!');
         document.querySelector('.quiz-questions').style.display = 'none';
-        document.querySelector('.asd').style.display = 'block';
+        document.querySelector('.last-question').style.display = 'block';
+        document.querySelector('.quiz__title').textContent = 'Ваша подборка готова!';
+        document.querySelector('.quiz__descr').textContent = 'Оставьте свои контактные данные, чтобы бы мы могли отправить  подготовленный для вас каталог';
+
+        document.querySelector('.quiz-form').addEventListener('submit', (e) => {
+          e.preventDefault();
+
+          quizFormData = new FormData();
+
+          console.log(this.resultArray);
+
+          for(let item of this.resultArray) {
+            for (let obj in item) {
+                
+              quizFormData.append(obj, item[obj].substring(0, item[obj].length - 1));
+            }
+          }
+
+          quizFormData.append('textarea', textareaText);
+
+          let xhr = new XMLHttpRequest();
+
+          xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+              if (xhr.status === 200) {
+                console.log('Отправлено');
+              }
+            }
+          }
+    
+          document.querySelector('.quiz-form').querySelectorAll('input').forEach(el => {
+            if (el.value) {
+              xhr.open('POST', 'mail.php', true);
+              xhr.send(quizFormData);  
+        
+              document.querySelector('.quiz-form').reset(); 
+            }
+          });     
+        });
       }
 		}
 	}
@@ -213,34 +246,13 @@ class Quiz {
 		this.resultArray.push(this.tmp);
 	}
 
-	send() {
-		if(this.valid()) {
-			console.log('send!');
-
-			let elements = this.$el.querySelectorAll('input');
-			elements.forEach(el => el.classList.remove('error'));
-
-
-			const formData = new FormData();
-
-			for(let item of this.resultArray) {
-				for (let obj in item) {
-					formData.append(obj, item[obj].substring(0, item[obj].length - 1))
-				}
-			}
-
-			const response = fetch('mail.php', {
-				method: 'POST',
-				body: formData
-			});
-		}
-	}
 
 	serialize(form) {
 		let field, s = {};
 		let valueString = '';
 		if (typeof form == 'object' && form.nodeName == "FORM") {
 			let len = form.elements.length;
+      console.log(form.elements)
 			for (let i = 0; i < len; i++) {
 				field = form.elements[i];
 				
